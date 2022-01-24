4daa64ee8fcf466a723b194e9c6a1f0484308471
makc9pa <2max2@mail.ru>
2022-01-24 23:14:35 +0300

    feat(lesson22): использовал делегирование событий для уменьшения количества обработчиков

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from "./modules/timer"
 import menu from "./modules/menu"
 import modal from "./modules/modal"
 import inputCheck from "./modules/inputCheck"
+import tabs from "./modules/tabs"
 
 timer('29 january 2022')
 menu()
 modal()
 inputCheck()
+tabs()
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,18 @@
 const menu = () => {
     const menuBtn = document.querySelector('.menu')
     const menu = document.querySelector('menu')
-    const closeBtn = menu.querySelector('.close-btn')
-    const menuItems = menu.querySelectorAll('ul>li>a')
 
     const handleMenu = () => {
         menu.classList.toggle('active-menu')
     }
 
+    menu.addEventListener('click', (e) => {
+        if (e.target.classList.contains('close-btn') || e.target.tagName === 'A') {
+            handleMenu()
+        }
+    })
+
     menuBtn.addEventListener('click', handleMenu)
-
-    closeBtn.addEventListener('click', handleMenu)
-
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
 }
 
 export default menu
